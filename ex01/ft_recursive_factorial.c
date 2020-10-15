cce5a9f3c6541dc1170d6d1c600d85011f92c2ff
Antoine Labalette <alabalet@student.42nice.fr>
2020-10-15 12:00:21 +0200

    [m] ex01 norme

--- ex01/ft_recursive_factorial.c
+++ ex01/ft_recursive_factorial.c
@@ -6,11 +6,11 @@
 /*   By: alabalet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/15 11:29:01 by alabalet          #+#    #+#             */
-/*   Updated: 2020/10/15 11:53:12 by alabalet         ###   ########.fr       */
+/*   Updated: 2020/10/15 11:59:56 by alabalet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_recursive_factorial(int nb)
+int	ft_recursive_factorial(int nb)
 {
 	if (nb < 0 || nb > 15)
 		return (0);
